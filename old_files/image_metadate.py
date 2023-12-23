6d5ec3bd9b7c1ff69014acf20e3ee26e205dd96a
“Caue <cauepaivalira@outlook.com>
2023-12-23 18:59:28 -0300

    limpeza de arquivos desnecessários

--- old_files/image_metadate.py
+++
@@ -1,8 +0,0 @@
-import pyexifinfo as pxi
-
-info = pxi.get_json("0Im0.png")
-
-
-for i in info:
-    for key, value in i.items():
-        print(f"key: {key},  value:{value},")
--- old_files/pdf_to_tree.py
+++
@@ -1,3 +0,0 @@
-import pdftotree
-
-html = pdftotree.parse('PDF_3_PAGINAS.pdf', html_path='document2.html', model_type=None)
